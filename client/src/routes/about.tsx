1e3287984a6aa21f849f9fb2264fbc73175f0753
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-12-08 23:22:33 -0500

    Fix pic size

--- client/src/routes/about.tsx
+++ client/src/routes/about.tsx
@@ -2,26 +2,24 @@ import aboutPic from "@/assets/images/about-pic.jpg";
 
 const AboutPage = () => {
   return (
-    <div className="container mx-auto my-10 px-4">
-      {/* Title */}
+    <div className="container mx-auto px-4 py-8 max-w-6xl">
       <h1 className="text-4xl font-bold text-center mb-8">About</h1>
 
-      {/* Content Section */}
-      <div className="flex flex-wrap md:flex-nowrap items-center bg-red-500 p-8 rounded-lg">
+      <div className="flex flex-col md:flex-row items-center gap-8 bg-white p-8 rounded-lg shadow-md border border-indigo-200">
         {/* Text Container */}
-        <div className="md:w-1/2 w-full pr-6 mb-6 md:mb-0">
-          <p className="text-lg text-white">
+        <div className="md:w-1/2 space-y-4">
+          <p className="text-xl text-gray-800 leading-relaxed">
             The app enables users to easily understand colloquial language
             through clear, accessible explanations.
           </p>
         </div>
 
         {/* Image Container */}
-        <div className="md:w-1/2 w-full">
+        <div className="md:w-1/2">
           <img
             src={aboutPic}
             alt="About"
-            className="w-full rounded-lg shadow-lg"
+            className="w-full max-h-[calc(100vh-12rem)] object-cover rounded-lg shadow-md"
           />
         </div>
       </div>
